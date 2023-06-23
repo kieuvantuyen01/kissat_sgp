4b77711b88f94a6ce559c22e7f83ec6a70578f7a
Ngoc Anh Pham <anhpham197@minh.nq2.sv>
2023-06-23 10:08:14 +0700

    update result for v3

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-ls input/binomial/cnf_noALO/*.cnf | xargs -n 1 ./build/kissat --time=1800 --relaxed | ./process.py > output/binomial/noALO.txt
+# ls input/binomial/cnf_noALO/*.cnf | xargs -n 1 ./build/kissat --time=1800 --relaxed | ./process.py > output/binomial/noALO.txt
+ls input/binomial/cnf_ALO/*.cnf | xargs -n 1 ./build/kissat --time=1800 | ./process.py > output_v3/binomial/ALO.txt
