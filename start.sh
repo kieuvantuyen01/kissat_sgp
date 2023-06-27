afc4d6528a6e99b26c0404dd92efb6051f50b0f4
anhpham197 <ngocanhpham197@gmail.com>
2023-06-27 22:45:49 +0700

    update result for binary

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # ls input/binomial/cnf_noALO/*.cnf | xargs -n 1 ./build/kissat --time=1800 --relaxed | ./process.py > output/binomial/noALO.txt
-ls input_v3/binary/cnf_noALO_v3/*.cnf | xargs -n 1 ./build/kissat --time=1800 | ./process.py > output_v3/binary/noALO.txt
+ls input_v3/binary/cnf_ALO_v3/*.cnf | xargs -n 1 ./build/kissat --time=1800 | ./process.py > output_v3/binary/ALO.txt
